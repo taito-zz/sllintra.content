9f7948cc0f144eca0107a092c85cc6108e2febd0
Taito Horiuchi <taito.horiuchi@gmail.com>
2015-06-17 15:51:38 +0300

    Fix test

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'Products.CMFPlacefulWorkflow',
         'collective.base',
         'collective.dexteritytextindexer',
         'plone.app.dexterity',
