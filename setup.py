3b4f75428cb64689c3a35d3c150840dd20701357
Taito Horiuchi <taito.horiuchi@gmail.com>
2015-07-29 16:32:45 +0300

    release 0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sllintra.content',
-    version='0.3',
+    version='0.4',
     description="Provides content types for SLL intra site.",
     long_description=open("README.rst").read(),
     classifiers=[
